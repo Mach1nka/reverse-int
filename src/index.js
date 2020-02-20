232b9162adf79c80f158617ed263ed17d7408802
Artem <uspenskiyart@gmail.com>
2020-02-20 19:36:19 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  const num = Math.abs(n)
+  let string = num.toString();
+  let arr = Array.from(string);
+  let arrRev = arr.reverse();
+  let resoult = arrRev.join('');
+  return +resoult;
 }
